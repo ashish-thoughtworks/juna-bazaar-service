c0a896111e2f09d74c427bf0348b6853a69e98be
Harihar Das <dharihar@thoughtworks.com>
2016-11-13 16:50:25 +0530

    Intial project structure with Gradle and SpringBoot

---
+++ src/main/java/com/junabazar/JunabazarApiApplication.java
@@ -0,0 +1,12 @@
+package com.junabazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunabazarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunabazarApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/junabazar/JunabazarApiApplicationTests.java
@@ -0,0 +1,19 @@
+package com.junabazar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class JunabazarApiApplicationTests {
+
+	@Test
+	public void shouldExecuteWithoutFail() {
+		assertEquals(1,1);
+	}
+
+}
