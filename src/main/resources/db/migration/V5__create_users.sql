f3530df627e35238a3cab3ffa8e8fe89707989ec
Harihar Das <dharihar@thoughtworks.com>
2016-11-15 00:03:53 +0530

    Modified column size for password in users table

--- src/main/resources/db/migration/V5__create_users.sql
+++ src/main/resources/db/migration/V5__create_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
 (
 	id bigint PRIMARY KEY NOT NULL,
 	email VARCHAR(100) NOT NULL,
-	password VARCHAR(30) NOT NULL,
+	password VARCHAR(200) NOT NULL,
 	mobile_no VARCHAR(10) NOT NULL,
 	city_id bigint NOT NULL REFERENCES cities(id)
 );
