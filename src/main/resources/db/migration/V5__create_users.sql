0f9107adea0c30d0bf316b2ab4537f91f0ac4062
Harihar Das <dharihar@thoughtworks.com>
2016-11-14 23:41:16 +0530

    Corrected user creation script. Also removed unnecessary config from application.properties file

--- src/main/resources/db/migration/V5__create_users.sql
+++ src/main/resources/db/migration/V5__create_users.sql
@@ -5,6 +5,6 @@ CREATE TABLE users
 	id bigint NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	password VARCHAR(30) NOT NULL,
-	password VARCHAR(10) NOT NULL,
+	mobile_no VARCHAR(10) NOT NULL,
 	city_id bigint NOT NULL REFERENCES cities(id)
 );
