e5a9920557def5462f3b089a5181748873e0d294
Sarthak Makhija <sarthakm@in-sarthakmakhija.local>
2016-11-14 21:02:19 +0530

    Added a configuration for enabling Corss Origin Requests. Currently Cross Origin Requests from all domains have been enabled

---
+++ src/main/java/com/junabazar/config/CrossOriginConfig.java
@@ -0,0 +1,24 @@
+package com.junabazar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by sarthakm on 11/14/16.
+ */
+@Configuration
+public class CrossOriginConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
