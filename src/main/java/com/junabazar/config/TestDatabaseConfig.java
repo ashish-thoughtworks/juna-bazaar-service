49f1cd6bff378cdd3d301b681c871a4d9ca5b833
Harihar Das <dharihar@thoughtworks.com>
2016-11-15 19:26:40 +0530

    Incorporated in memory db for integration testing

--- src/main/java/com/junabazar/config/TestDatabaseConfig.java
+++ src/main/java/com/junabazar/config/TestDatabaseConfig.java
@@ -1,12 +1,19 @@
 package com.junabazar.config;
 
 
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
@@ -16,16 +23,39 @@ import javax.sql.DataSource;
 @Configuration
 @Profile("service-test")
 public class TestDatabaseConfig {
+
+    @Value("classpath:schema.sql")
+    private Resource SCHEMA_SCRIPT;
+
+    @Value("classpath:city_data.sql")
+    private Resource CITY_DATA_SCRIPT;
+
+    @Value("classpath:user_data.sql")
+    private Resource USER_DATA_SCRIPT;
+
     @Bean
-    @Primary
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .generateUniqueName(true)
-                .setType(EmbeddedDatabaseType.HSQL)
-                .setScriptEncoding("UTF-8")
-                .ignoreFailedDrops(true)
-                .addScript("db/test/schema.sql")
-                .addScripts("db/test/city_data.sql", "db/test/user_data.sql")
-                .build();
+    public DataSourceInitializer dataSourceInitializer() throws Exception {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(createH2DataSource());
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(SCHEMA_SCRIPT);
+        populator.addScripts(CITY_DATA_SCRIPT, USER_DATA_SCRIPT);
+        return populator;
+    }
+
+    private DataSource createH2DataSource() {
+        String jdbcUrl = "jdbc:hsqldb:mem:testdb";
+        JDBCDataSource ds = new JDBCDataSource();
+        ds.setURL(jdbcUrl);
+        ds.setUser("sa");
+        ds.setPassword("");
+
+        return ds;
     }
 }
--- src/test/java/com/junabazar/JunabazarApiApplicationTests.java
+++ src/test/java/com/junabazar/JunabazarApiApplicationTests.java
@@ -1,9 +1,6 @@
 package com.junabazar;
 
 import com.jayway.restassured.RestAssured;
-import com.junabazar.inventory.model.db.User;
-import com.junabazar.inventory.repository.UserRepository;
-import org.apache.catalina.core.ApplicationContext;
 import org.apache.http.HttpStatus;
 
 import static org.hamcrest.Matchers.*;
@@ -11,29 +8,25 @@ import static org.hamcrest.Matchers.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.jayway.restassured.RestAssured.*;
-//import static com.
 
 import static org.junit.Assert.assertEquals;
 
 @ActiveProfiles("service-test")
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = JunabazarApiApplication.class)
 @IntegrationTest("server.port:0")
 @TestPropertySource(properties = "flyway.enabled=false")
 public class JunabazarApiApplicationTests {
 
-    @Value("${local.server.port}")   // 6
+    @Value("${local.server.port}")
     private int port;
 
     @Before
