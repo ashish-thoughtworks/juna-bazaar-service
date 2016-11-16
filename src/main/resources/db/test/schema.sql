fa0f42a57f7da3a33526ac31f1f5ac710372026c
Harihar Das <dharihar@thoughtworks.com>
2016-11-16 19:13:31 +0530

    fixed test cases

--- src/main/resources/db/test/schema.sql
+++ src/main/resources/db/test/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE users
 	email VARCHAR(100) NOT NULL,
 	password VARCHAR(200) NOT NULL,
 	mobile_no VARCHAR(10) NOT NULL,
-	city_id bigint NOT NULL REFERENCES cities(id)
+	city_id bigint NOT NULL REFERENCES cities(id),
+	name VARCHAR
 );
 
 CREATE TABLE categories
